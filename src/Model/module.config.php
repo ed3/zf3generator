761fd1dbb7ca9ae435f1f224b725ff0956f05cd7
Ed <ed3@users.noreply.github.com>
2017-04-07 20:00:14 +0300

    Create module.config.php

---
+++ src/Model/module.config.php
@@ -0,0 +1,14 @@
+<?php
+return [
+	'controllers' => [
+		'invokables' => [
+		],
+	],
+	'router' => [
+		'routes' => [
+		],
+	],
+	'view_manager' => [
+	'template_path_stack' => ['__NAMESPACE__' => '__DIR__ ". /../view"'],
+	],
+];
